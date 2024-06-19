a34a9e7ac70e2215dd64ba1efe0dce8261361e25
Krishna Kant Chourasiya <lushkant@users.noreply.github.com>
2024-06-19 23:05:50 +0530

    Remove Freemius

--- really-simple-featured-audio.php
+++ really-simple-featured-audio.php
@@ -23,45 +23,6 @@ define( 'RSFA_PLUGIN_DIR', plugin_dir_path( RSFA_PLUGIN_FILE ) );
 define( 'RSFA_PLUGIN_BASE', plugin_basename( RSFA_PLUGIN_FILE ) );
 define( 'RSFA_PLUGIN_PRO_URL', 'https://jetixwp.com/plugins/really-simple-featured-audio' );
 
-if ( ! function_exists( 'rsfa_fs' ) ) {
-	/**
-	 * Create a helper function for easy SDK access.
-	 */
-	function rsfa_fs() {
-		global $rsfa_fs;
-
-        if ( ! isset( $rsfa_fs ) ) {
-            // Include Freemius SDK.
-            require_once __DIR__ . '/freemius/start.php';
-
-            $rsfa_fs = fs_dynamic_init( array(
-                'id'                  => '15832',
-                'slug'                => 'really-simple-featured-audio',
-                'type'                => 'plugin',
-                'public_key'          => 'pk_966ab730c951fb6730786c41ce9ad',
-                'is_premium'          => false,
-                'has_addons'          => false,
-                'has_paid_plans'      => false,
-                'menu'                => array(
-                    'slug'           => 'rsfa-settings',
-                    'account'        => false,
-                    'support'        => false,
-                    'parent'         => array(
-                        'slug' => 'options-general.php',
-                    ),
-                ),
-            ) );
-        }
-
-        return $rsfa_fs;
-	}
-
-	// Init Freemius.
-	rsfa_fs();
-	// Signal that SDK was initiated.
-	do_action( 'rsfa_fs_loaded' );
-}
-
 /**
  * Fire up plugin instance.
  */
