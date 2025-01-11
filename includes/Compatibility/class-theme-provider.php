f52f2b547eec45015042d0ad2d851cd214e569fb
Krishna Kant Chourasiya <lushkant@users.noreply.github.com>
2025-01-11 22:26:44 +0530

    Update theme provider class for engine promo

--- includes/Compatibility/class-theme-provider.php
+++ includes/Compatibility/class-theme-provider.php
@@ -171,6 +171,27 @@ class Theme_Provider {
 			$selectable_engines[ $engine_id ] = $engine_data['title'];
 		}
 
+		// Pro theme Engines for promo.
+		$pro_selectable_engines = $this->get_selectable_pro_engine_options_promo();
+
+		// Include promo engines.
+		foreach ( $pro_selectable_engines as $engine_id => $engine_label ) {
+			if ( ! array_key_exists( $engine_id, $selectable_engines ) ) {
+				$selectable_engines[ $engine_id ] = $engine_label;
+			}
+		}
+
 		return $selectable_engines;
+	}
+
+	/**
+	 * Returns the list of theme engines available in PRO plugin.
+	 *
+	 * @return array
+	 */
+	public function get_selectable_pro_engine_options_promo() {
+		return array(
+			'divi' => __( 'Divi Builder (PRO)', 'really-simple-featured-audio' ),
+		);
 	}
 }
