9e9352e0e3efb0d7ff46bfce20f242fb0f1e21eb
Krishna Kant Chourasiya <lushkant@users.noreply.github.com>
2025-01-31 19:53:43 +0530

    Fix Blog archives audio toggle

--- includes/class-frontend.php
+++ includes/class-frontend.php
@@ -8,6 +8,7 @@
 namespace RSFA;
 
 use function RSFA\Settings\get_post_types;
+use RSFA\Options;
 
 /**
  * Class FrontEnd
@@ -119,6 +120,11 @@ class FrontEnd {
 	public function get_post_audio( $html, $post_id, $post_thumbnail_id, $size, $attr ) {
 		global $post;
 
+		$options                  = Options::get_instance();
+		$blog_archives_visibility = $options->get( 'blog_archives_visibility' );
+		if ( ( ( is_home() || is_archive() ) && ( $options->has( 'blog_archives_visibility' ) && ! $blog_archives_visibility ) ) ) {
+			return $html;
+		}
 		if ( 'object' !== gettype( $post ) ) {
 			return $html;
 		}
