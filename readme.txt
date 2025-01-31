5379063aeeb37800f4bfcfc954a761ba721192e5
Krishna Kant Chourasiya <lushkant@users.noreply.github.com>
2025-01-31 20:28:06 +0530

    Release patch

--- readme.txt
+++ readme.txt
@@ -3,7 +3,7 @@ Contributors: jetixwp, lushkant
 Requires at least: 6.0
 Requires PHP: 8.0
 Tested up to: 6.7.1
-Stable tag: 1.0.4
+Stable tag: 1.0.5
 Tags: audio, podcast, woocommerce, featured, music
 License: GPL v2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
@@ -64,6 +64,9 @@ Well Yes and No, So Yes as long as the theme you use follows standard WordPress/
 You can get support at the forums here or via support@jetixwp.com.
 
 == Changelog ==
+
+= 1.0.5 =
+- Improvement: Patch release
 
 = 1.0.4 =
 - New: Settings toggle for Blog Single
--- really-simple-featured-audio.php
+++ really-simple-featured-audio.php
@@ -3,7 +3,7 @@
  * Plugin Name: Really Simple Featured Audio
  * Plugin URI:  https://jetixwp.com/plugins/really-simple-featured-audio
  * Description: Adds support for Featured Audio to WordPress posts, pages & WooCommerce products.
- * Version:     1.0.4
+ * Version:     1.0.5
  * Author:      JetixWP Plugins
  * Author URI:  https://jetixwp.com
  * License:     GPL2
@@ -16,7 +16,7 @@
 
 defined( 'ABSPATH' ) || exit;
 
-define( 'RSFA_VERSION', '1.0.4' );
+define( 'RSFA_VERSION', '1.0.5' );
 define( 'RSFA_PLUGIN_FILE', __FILE__ );
 define( 'RSFA_PLUGIN_URL', plugin_dir_url( RSFA_PLUGIN_FILE ) );
 define( 'RSFA_PLUGIN_DIR', plugin_dir_path( RSFA_PLUGIN_FILE ) );
