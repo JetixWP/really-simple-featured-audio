1a58ff6043f3ca0819fbc4ccd9a886dfbdf5bf5d
Krishna Kant Chourasiya <lushkant@users.noreply.github.com>
2025-09-19 01:18:00 +0530

    Add promo at settings sidebar

--- includes/Settings/Views/html-admin-settings.php
+++ includes/Settings/Views/html-admin-settings.php
@@ -71,27 +71,43 @@ if ( ! $tab_exists ) {
 
 				<div class="sidebar">
 					<?php if ( ! class_exists( '\RSFA_Pro\Plugin' ) ) : ?>
-					<div class="help-box">
-						<div>
-							<h3>👋 Thank you for using our plugin!</h3>
-							<p class="desc">We are currently looking for user feedback to improve it further for most use cases. If you have something to suggest, please feel free to drop your request at - <a href="https://wordpress.org/support/plugin/really-simple-featured-audio/" target="_blank">the plugin support forum</a>.</p>
-						</div>
-						<div>
-							<p class="desc"><strong>If you find this useful,</strong> we really appreciate a review from you and it also helps us understand how you feel about this plugin - <a class="desc" href="https://wordpress.org/support/plugin/really-simple-featured-audio/reviews/#new-post" target="_blank">Review this plugin</a>
-							</p>
-						</div>
+						<div class="help-box">
+							<div>
+								<h3>🚀 &nbsp;Grab the PRO version with a Lifetime Deal</h3>
+								<p class="desc">Our RSFA Pro plugin is now available to support additional features, where we continue to keep them maintained and updated. We are also continuously working on adding more and more theme support.</p>
+							</div>
+							<div>
+								<p class="desc"><strong>Our Pro plugin includes these features -</strong></p>
+								<ul>
+									<li>✅ <strong>Priority Support</strong></li>
+									<li>✅ <strong>Change Audio Aspect Ratio</strong></li>
+									<li>✅ <strong>Change Audio Order at Woo Product CPT</strong></li>
+									<li>✅ <strong>Support for more Premium/Custom Themes</strong></li>
+									<li>✅ <strong>Requests for Theme Compatibility</strong></li>
+								</ul>
+							</div>
+							<div>
+								<p class="desc">New features in every update, to name a few upcoming changes -</p>
+								<ul>
+									<li>🏗️ Customize Appearance [Coming Soon]</li>
+									<li>🏗️ Branding/White label [Coming Soon]</li>
+									<li>🏗️ Featured Audio Blocks for Full Site Editing </li>
+									<li>🏗️ And more Audio related customizations</li>
+								</ul>
+							</div>
 
-						<div>
-							<p class="desc">If you like this plugin, you will absolutely love our other plugins.</p>
+							<div>
+								<a class="button button-primary" href="https://jetixwp.com/plugins/really-simple-featured-audio?utm_campaign=settings-sidebar&utm_source=rsfa-plugin" target="_blank">✨ Unlock Extra features with PRO</a>
+							</div>
+
+							<div>
+								<p><em>If you like our free plugin, you will absolutely love the PRO version. Thank you for using RSFA again, you are not just any supporter but truly the founders of our small business.</em></p>
+								<p><strong>Krishna</strong>, Founder and Lead Developer</p>
+
+								<p><strong>Have questions?</strong> Please mail us at <a href="mailto:hello@jetixwp.com">hello@jetixwp.com</a>, and we will get back to you at the earliest :)</p>
+
+							</div>
 						</div>
-						<div>
-							<a class="button button-primary" href="https://jetixwp.com/plugins?utm_campaign=settings-sidebar&utm_source=rsfa-plugin" target="_blank">✨ View all Plugins</a>
-						</div>
-						<div>
-							<p><em>Thank you for using RSFA again, you are not just any supporter but truly the founders of our small but mighty product agency.</em></p>
-							<p><strong>Krishna</strong>, Founder and Lead Developer at JetixWP</p>
-						</div>
-					</div>
 					<?php endif; ?>
 					<?php do_action( 'rsfa_extend_settings_sidebar' ); ?>
 				</div>
