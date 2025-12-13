3eca3fd3b35111b44fc454d415ce08251624fd92
Krishna Kant Chourasiya <lushkant@users.noreply.github.com>
2025-12-14 00:54:51 +0530

    Add required plugin header

--- really-simple-featured-audio.php
+++ really-simple-featured-audio.php
@@ -10,6 +10,8 @@
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: really-simple-featured-audio
  * Domain Path: /languages/
+ * Requires at least: 6.0
+ * Requires PHP: 8.0
  *
  * @package RSFA
  */
